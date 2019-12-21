ad9111ff98da729c6fe68fb2fa3459984d3dafee
SvyatKarelin <Vsr1234567!>
2019-12-21 12:34:52 +0300

    Ilya train/test photos added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from recognize import FaceRecognition
 
 
 fr = FaceRecognition(0.5)
-fr.recognize("images/20190804_204821.jpg")
+#fr.recognize("images/20190804_204821.jpg")
+#fr.recognize("images/IMG_7022.JPG")
+fr.recognize("images/IMG_8169.jpg")
 
 
