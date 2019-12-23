591b7ce60f5deb232d941066e96759fb90d3b8f4
SvyatKarelin <Vsr1234567!>
2019-12-23 23:38:07 +0300

    Video processing added

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from recognize import FaceRecognition
 
 
-fr = FaceRecognition(0.5)
+fr = FaceRecognition(0.6)
 #fr.recognize("images/20190804_204821.jpg")
 #fr.recognize("images/IMG_7022.JPG")
-fr.recognize("images/IMG_8169.jpg")
 
+#fr.recognizeImage("images/IMG_8169.jpg")
 
+fr.recognizeVideo()
--- recognize.py
+++ recognize.py
@@ -1,3 +1,7 @@
+# coding=utf-8
+from imutils.video import VideoStream
+from imutils.video import FPS
+import time
 import numpy as np
 import imutils
 import pickle
@@ -19,6 +23,7 @@ class FaceRecognition:
 		# загружаем веса в формате caffe
 		modelPath = os.path.sep.join(["face_detection_model/", "res10_300x300_ssd_iter_140000.caffemodel"])
 		self.detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+		self.detections = []
 
 		print("[INFO] загружаем модель нейросети...")
 		self.embedder = cv2.dnn.readNetFromTorch("openface_nn4.small2.v1.t7")
@@ -32,32 +37,69 @@ class FaceRecognition:
 		self.recognizer = pickle.loads(open("output/recognizer.pickle", "rb").read())
 		self.le = pickle.loads(open("output/le.pickle", "rb").read())
 
-	def recognize(self, image):
-		# загружаем тестовую картинку и ресайхим ее к размеру 600х600
+	def recognizeImage(self, image):
+		# загружаем тестовую картинку и ресайзим ее к размеру 600х600
 		image = cv2.imread(image)
-		image = imutils.resize(image, width=600)
-		(h, w) = image.shape[:2]
+		image = imutils.resize(image, width=600)\
 
+		self.processImage(image)
+
+		# выводим результат на экран
+		cv2.imshow("Image", image)
+		cv2.waitKey(0)
+
+	def recognizeVideo(self):
+		print("[INFO] запускаем камеру...")
+		vs = VideoStream(src=0).start()
+		# пауза две секунды на прогрев и включение
+		time.sleep(2.0)
+
+		# start the FPS throughput estimator
+		fps = FPS().start()
+
+		while True:
+			# захватываем видеокадр
+			image = vs.read()
+			# ресайзим его к размеру 600х600
+			image = imutils.resize(image, width=600)
+
+			self.processImage(image)
+
+			# update the FPS counter
+			fps.update()
+
+			# show the output frame
+			cv2.imshow("Frame", image)
+			key = cv2.waitKey(1) & 0xFF
+
+			# if the `q` key was pressed, break from the loop
+			if key == ord("q"):
+				break
+
+
+	def processImage(self, img):
 		imageBlob = cv2.dnn.blobFromImage(
-			cv2.resize(image, (300, 300)), 1.0, (300, 300),
+			cv2.resize(img, (300, 300)), 1.0, (300, 300),
 			(104.0, 177.0, 123.0), swapRB=False, crop=False)
 
 		# отдаем картинку определителю лиц
 		self.detector.setInput(imageBlob)
-		detections = self.detector.forward()
+		self.detections = self.detector.forward()
+
+		(h, w) = img.shape[:2]
 
 		# обрабатываем все найденные прямоугольники с лицами
-		for i in range(0, detections.shape[2]):
+		for i in range(0, self.detections.shape[2]):
 			# берем текущую вероятность того, что это лицо
-			confidence = detections[0, 0, i, 2]
+			confidence = self.detections[0, 0, i, 2]
 
 			# отбрасываем лица с вероятностью меньше пороговой (0.5 по умолчанию)
 			if confidence > self.confidence:
 				# вырезаем прямоугольник с лицом из картинки
-				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+				box = self.detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 				(startX, startY, endX, endY) = box.astype("int")
 
-				face = image[startY:endY, startX:endX]
+				face = img[startY:endY, startX:endX]
 				(fH, fW) = face.shape[:2]
 
 				# лица размером меньше 20х20 не рассматриваем
@@ -67,7 +109,7 @@ class FaceRecognition:
 				# создаем из лица blob и отправляем его в модель
 				# для получения 128-мерной гиперсферы с просчитанными векторами
 				faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255, (96, 96),
-					(0, 0, 0), swapRB=True, crop=False)
+												 (0, 0, 0), swapRB=True, crop=False)
 				self.embedder.setInput(faceBlob)
 				vec = self.embedder.forward()
 
@@ -81,11 +123,7 @@ class FaceRecognition:
 				# к предсказанному классу
 				text = "{}: {:.2f}%".format(name, proba * 100)
 				y = startY - 10 if startY - 10 > 10 else startY + 10
-				cv2.rectangle(image, (startX, startY), (endX, endY),
-					(0, 0, 255), 2)
-				cv2.putText(image, text, (startX, y),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-
-		# выводим результат на экран
-		cv2.imshow("Image", image)
-		cv2.waitKey(0)
+				cv2.rectangle(img, (startX, startY), (endX, endY),
+							  (0, 0, 255), 2)
+				cv2.putText(img, text, (startX, y),
+							cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
